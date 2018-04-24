dc22b7fbb9e115d89b36c9bdb10ce9f6dbcf12eb
Sylvain R <sylvainriondet@gmail.com>
2018-04-25 03:03:11 +0800

    added Formula class, functions to create clauses and terms, fixing __repr__

--- sat_classes.py
+++ sat_classes.py
@@ -8,26 +8,59 @@
 # #######################################################################################
 
 
-class Clause:
-    """ List of terms in SAT """
-    nb_clauses = 0
+class Formula:
+    """ CNF Formula: list of clauses """
 
-    def __init__(self, index, terms):
+    def __init__(self):
+        self.nb_terms = 0
+        self.nb_clauses = 0
+        self.clauses = []
+        self.terms = []
+
+    def create_terms(self):
+        self.terms = [Term(x) for x in range(self.nb_terms)]
+
+    def create_clauses(self):
+        self.clauses = [Clause(x) for x in range(self.nb_clauses)]
+
+    def __repr__(self):
+        return f"Formula consists of the following list of clauses: \n" + "\n".join([str(c) for c in self.clauses])
+
+
+class Clause:
+    """ List of terms in SAT
+        starting index at 1
+        self.terms should be [{sign: +/-1, val:Term}, {}]
+    """
+    nb_clauses_count = 0
+
+    def __init__(self, index):
         self.index = index
-        self.terms = terms
+        self.terms = []
+        self.nb_clauses_count += 1
+
+    def __repr__(self):
+        return f"* Clause {self.index + 1} with Terms: \n\t" + "\n\t".join([str(t) for t in self.terms])
 
 
 class Term:
-    """ Term, with an id (x1, x2, ...) and a value (+1 or 0) """
-    nb_terms = 0    # total nb of terms
+    """ Term, with an id (x1, x2, ...) and a sign (+1 or -1) """
+    tot_nb_terms = 0    # total nb of terms
+    nb_terms_count = 0
+    values = []
 
-    def __init__(self, x, val=None):
+    def __init__(self, x, sign=None):
         self.x = x
-        self.val = val
-        Term.nb_terms += 1
+        self.sign = sign
+        Term.tot_nb_terms += 1
 
     def __repr__(self):
-        return f"Term x{self.x} = {self.val}"
+        if self.sign == 1:
+            return f"Term +{self.x}"
+        elif self.sign == -1:
+            return f"Term -{self.x}"
+        else:
+            return f"Term  {self.x}"
 
 
 
